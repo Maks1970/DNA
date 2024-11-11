c4a87116b6e2834437894af900dd016af9a2bc18
Max Kosh <koshelmak@gmail.com>
2024-11-11 16:24:32 +0200

    Changing the compression and decompression functions

--- DNA/Program.cs
+++ DNA/Program.cs
@@ -4,192 +4,79 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Text;
 
 namespace DNA
 {
     internal class Program
     {
 
-
-        public static List<KeyValuePair<char, int>> compress(string dna)
+        public static byte[] compress(string dna)
         {
-            char A = 'A';
-            char G = 'G';
-            char C = 'C';
-            char T = 'T';
-            int k = 0;
-            List<KeyValuePair<char, int>> dictionary = new List<KeyValuePair<char, int>>();
+            List<byte> compressed = new List<byte>();
 
-            for (int i = 0; i < dna.Length; i++)
+            for (int i = 0; i < dna.Length; i += 4)
             {
+                byte compressedByte = 0;
 
-                if (dna[i] == A)
+                for (int j = 0; j < 4 && i + j < dna.Length; j++)
                 {
-                    for (; i < dna.Length; i++)
+                    byte nucleotideBits = dna[i + j] switch
                     {
-                        if (dna[i] == A)
-                        {
-                            k++;
-                        }
-                        else break; 
-                    }
-                    //    while (i < dna.Length && dna[i] == A)
-                    //{
-                    //    i++;
-                    //    k++;
+                        'A' => 0b00,
+                        'C' => 0b01,
+                        'G' => 0b10,
+                        'T' => 0b11,
+                        _ => throw new ArgumentException("Недійсний символ в ДНК-ланцюгу.")
+                    };
 
-                    //}
-                    //i--;
-                    dictionary.Add(new KeyValuePair<char, int>(A, k));
-                    k = 0;
-                    if (i >= dna.Length) break;
+                    compressedByte |= (byte)(nucleotideBits << (6 - 2 * j));
                 }
 
-                if (dna[i] == G)
-                {
-                    while (i < dna.Length && dna[i] == G)
-                    {
-                        i++;
-                        k++;
-
-                    }
-                    i--;
-                    dictionary.Add(new KeyValuePair<char, int>(G, k));
-                    k = 0;
-                    if (i >= dna.Length) break;
-                }
-                if (dna[i] == C)
-                {
-                    while (i < dna.Length && dna[i] == C)
-                    {
-                        i++;
-                        k++;
-
-                    }
-                    i--;
-                    dictionary.Add(new KeyValuePair<char, int>(C, k));
-                    k = 0;
-                    if (i >= dna.Length) break;
-                }
-                if (dna[i] == T)
-                {
-                    while (i < dna.Length && dna[i] == T)
-                    {
-                        i++;
-                        k++;
-
-                    }
-                    i--;
-                    dictionary.Add(new KeyValuePair<char, int>(T, k));
-                    k = 0;
-                    if (i >= dna.Length) break;
-                }
-
-
-
+                compressed.Add(compressedByte); 
             }
-            return (dictionary);
+
+            return compressed.ToArray(); 
         }
 
 
-       public static List<char> decompress(string comdna)
+        public static string DecompressDNA(byte[] compressedData)
         {
-            List<char> list = new List<char>();
-            char A = 'A';
-            char G = 'G';
-            char C = 'C';
-            char T = 'T';
-            int num = 0;
-            int Numeric = 0;
-            for (int i = 0; i < comdna.Length; i++)
+            StringBuilder decompressedDNA = new StringBuilder();
+
+            foreach (byte compressedByte in compressedData)
             {
-                
-                if ((comdna[i] == A) )
+           
+                for (int i = 0; i < 4; i++)
                 {
-                         i++;
-                         num = comdna[i];
-                        if ((num <= 57) && (num > 48))
-                        {
-                            Numeric = num - '0';
-                        }
-                        for (int j = 0; j<Numeric; j++)
-                        {
-                            list.Add(A);
-                        }
-                       
-                }
+                    
+                    byte nucleotideBits = (byte)((compressedByte >> (6 - 2 * i)) & 0b11);
+                 
+                    char nucleotide = nucleotideBits switch
+                    {
+                        0b00 => 'A',
+                        0b01 => 'C',
+                        0b10 => 'G',
+                        0b11 => 'T',
+                        _ => throw new InvalidOperationException("Невідомий бітовий код")
+                    };
 
-                if ((comdna[i] == G))
-                {
-                    i++;
-                    num = comdna[i];
-                    if ((num <= 57) && (num > 48))
-                    {
-                        Numeric = num - '0';
-                    }
-                    for (int j = 0; j < Numeric; j++)
-                    {
-                        list.Add(G);
-                    }
-                  
-                }
-                if ((comdna[i] == C))
-                {
-                    i++;
-                    num = comdna[i];
-                    if ((num <= 57) && (num > 48))
-                    {
-                        Numeric = num - '0';
-                    }
-                    for (int j = 0; j < Numeric; j++)
-                    {
-                        list.Add(C);
-                    }
-
-                }
-                if ((comdna[i] == T))
-                {
-                    i++;
-                    num = comdna[i];
-                    if ((num <= 57) && (num > 48))
-                    {
-                        Numeric = num - '0';
-                    }
-                    for (int j = 0; j < Numeric; j++)
-                    {
-                        list.Add(T);
-                    }
-
+                    decompressedDNA.Append(nucleotide);
                 }
             }
-            return list;
+
+            return decompressedDNA.ToString();
         }
-            static void Main(string[] args)
+
+        static void Main(string[] args)
         {
             
-            string dn = "AACCAGGGAAACCCTTTCCGGTTTTAAAA"; //
-           // string ddn = "A2C2A1G3A3C3T3C2G2T4A4";
-            dn = Console.ReadLine();
-            int nu = dn[1];
-            if ((nu <= 57) && (nu > 48))
-            {
-                List<Char> decom = new List<Char>();
-                decom = decompress(dn);
-                string result = string.Join("", decom);
-                Console.WriteLine(result);
-            }
-            else 
-            {
-                List<KeyValuePair<char, int>> dic = new List<KeyValuePair<char, int>>();
-                dic = compress(dn);
-                foreach (KeyValuePair<char, int> kvp in dic)
-                {
-                    Console.Write($"{kvp.Key}{kvp.Value}");
-                }
-                Console.WriteLine();
-            }
-            
-            
+            string dn = "AACCAGGG"; 
+            byte[] compres = compress(dn);
+            Console.WriteLine("Стисненi данi: " + BitConverter.ToString(compres));
+            Console.WriteLine("Данi: " + DecompressDNA(compres));
+
+
         }
     }
 }
